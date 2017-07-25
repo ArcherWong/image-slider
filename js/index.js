506f37890083df0e8f89e6644462fcd96c8cbc68
Jiechang Wang <249280082@qq.com>
2017-07-25 20:31:24 +0800

    demo

---
+++ js/index.js
@@ -0,0 +1,3 @@
+var imageWrapperElement = document.getElementsByClassName('image-wrapper')[0];
+var currentImageElement = document.getElementsByClassName('current-image')[0];
+slider(imageWrapperElement, currentImageElement);
